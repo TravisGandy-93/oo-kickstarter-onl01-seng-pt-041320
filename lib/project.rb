e17eb4ed745a9569cc23bb3b8d86ac87b83fc228
Travis Gandy <travis_gandy@yahoo.com>
2020-05-12 22:30:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,6 +5,9 @@ class Project
     @title = title
     @backers = []
   end 
-  
-  
+  def add_backer(backer)
+      backer = Backer.new(backer)
+      @backers << backer
+      @backers
+  end 
 end 
